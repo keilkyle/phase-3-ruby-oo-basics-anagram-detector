b8a74f0b2ab20670619ab8d391e01b1b59fe9b93
keilkyle <98378148+keilkyle@users.noreply.github.com>
2022-06-08 19:26:55 -0700

    commit tree 7bf4c6ba25aa33ca28b7842f056948a4c5c451d5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.filter do |oneWord|
+            oneWord.chars.sort == @word.chars.sort
+        end
+    end
+
+end
